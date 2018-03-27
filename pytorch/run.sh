8f6f364708faeaf13a0d76c6a561f31b438b6deb
Mingfei Ma <mingfei.ma@intel.com>
2018-03-27 10:26:23 +0800

    add inference only mode

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+ARGS=""
+if [[ "$1" == "--inference" ]]; then
+    ARGS="$ARGS --inference"
+    echo "### inference only"
+    shift
+fi
+
 CORES=`lscpu | grep Core | awk '{print $4}'`
 SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
 TOTAL_CORES=`expr $CORES \* $SOCKETS`
@@ -12,5 +19,5 @@ export $KMP_SETTING
 echo -e "### using OMP_NUM_THREADS=$TOTAL_CORES"
 echo -e "### using $KMP_SETTING\n"
 
-python -u benchmark.py
+python -u benchmark.py $ARGS
 

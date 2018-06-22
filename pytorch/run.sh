051a2ffdf5bcc9a7e27c119ec1ee691eb452b866
Mingfei Ma <mingfei.ma@intel.com>
2018-06-22 15:52:57 +0800

    add new KMP_BLOCKTIME setting

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -18,12 +18,14 @@ SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
 TOTAL_CORES=`expr $CORES \* $SOCKETS`
 
 KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+KMP_BLOCKTIME=1
 
 export OMP_NUM_THREADS=$TOTAL_CORES
 export $KMP_SETTING
+export KMP_BLOCKTIME=$KMP_BLOCKTIME
 
 echo -e "### using OMP_NUM_THREADS=$TOTAL_CORES"
-echo -e "### using $KMP_SETTING\n"
+echo -e "### using $KMP_SETTING"
+echo -e "### using KMP_BLOCKTIME=$KMP_BLOCKTIME\n"
 
 python -u benchmark.py $ARGS
-

b7ee71d0761faa95049e1b3779bbc118219783f1
Mingfei Ma <mingfei.ma@intel.com>
2018-09-05 13:53:45 +0800

    remove some warnings

--- pytorch/shufflenet.py
+++ pytorch/shufflenet.py
@@ -227,16 +227,16 @@ class ShuffleNet(nn.Module):
     def init_params(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                init.kaiming_normal(m.weight, mode='fan_out')
+                init.kaiming_normal_(m.weight, mode='fan_out')
                 if m.bias is not None:
-                    init.constant(m.bias, 0)
+                    init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
-                init.constant(m.weight, 1)
-                init.constant(m.bias, 0)
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
             elif isinstance(m, nn.Linear):
-                init.normal(m.weight, std=0.001)
+                init.normal_(m.weight, std=0.001)
                 if m.bias is not None:
-                    init.constant(m.bias, 0)
+                    init.constant_(m.bias, 0)
 
 
     def _make_stage(self, stage):
